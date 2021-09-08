da4e36787cd10bddd688e028feecf0762be20512
sam <wax654@gmail.com>
2021-09-08 16:33:16 -0700

    trying something ne w

--- server/db.js
+++ server/db.js
@@ -4,7 +4,11 @@ const { Client } = require("pg");
 const { DB_URI } = require("./config");
 
 let db = new Client({
-    connectionString: DB_URI
+    connectionString: DB_URI,
+    //unfortunately neccessay for Heroku
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 db.connect();
